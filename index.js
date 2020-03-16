4ef10cd152a38cdbbe09b9cf85d76e788ae66cc2
miti99 <minhtienit99@gmail.com>
2020-03-16 15:44:07 +0700

    Update

--- index.js
+++ index.js
@@ -7,6 +7,96 @@ const
   body_parser = require('body-parser'),
   app = express().use(body_parser.json());
 
+const math = [
+  {
+    "q": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "0 + 1 = ?",
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "1",
+              "payload": "Math_0_A"
+            },
+            {
+              "type": "postback",
+              "title": "2",
+              "payload": "Math_0_B"
+            },
+            {
+              "type": "postback",
+              "title": "3",
+              "payload": "Math_0_C"
+            }
+          ]
+        }
+      }
+    },
+    "a": "A"
+  },
+  {
+    "q": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "1 + 1 = ?",
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "1",
+              "payload": "Math_1_A"
+            },
+            {
+              "type": "postback",
+              "title": "2",
+              "payload": "Math_1_B"
+            },
+            {
+              "type": "postback",
+              "title": "3",
+              "payload": "Math_1_C"
+            }
+          ]
+        }
+      }
+    },
+    "a": "B"
+  },
+  {
+    "q": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "1 + 2 = ?",
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "1",
+              "payload": "Math_2_A"
+            },
+            {
+              "type": "postback",
+              "title": "2",
+              "payload": "Math_2_B"
+            },
+            {
+              "type": "postback",
+              "title": "3",
+              "payload": "Math_2_C"
+            }
+          ]
+        }
+      }
+    },
+    "a": "C"
+  }
+];
+
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.post('/webhook', (req, res) => {
@@ -20,18 +110,16 @@ app.post('/webhook', (req, res) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
-
       let sender_psid = webhook_event.sender.id;
       console.log('Sender ID: ' + sender_psid);
 
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
-
         handlePostback(sender_psid, webhook_event.postback);
       }
-
     });
+
     res.status(200).send('EVENT_RECEIVED');
 
   } else {
@@ -63,9 +151,10 @@ function handleMessage(sender_psid, received_message) {
   let response;
 
   if (received_message.text) {
-    response = {
-      "text": `Bạn đã gửi: "${received_message.text}" cho chúng tôi. Hiện tại chatbot đang được phát triển, chưa có chức năng phản hồi!`
-    };
+    console.log($received_message.text);
+    // response = {
+    //   "text": `Bạn đã gửi: "${received_message.text}" cho chúng tôi. Hiện tại chatbot đang được phát triển, chưa có chức năng phản hồi!`
+    // };
   }
   callSendAPI(sender_psid, response);
 }
@@ -74,81 +163,35 @@ function handlePostback(sender_psid, received_postback) {
   console.log('ok');
   let response;
   let payload = received_postback.payload;
-  if (payload === 'ViNiX') {
+  if (payload === 'GetStarted') {
+    // response = {
+    //   // "text": "Chào mừng bạn đến với BK FC THPT Đức Hoà! Mọi vấn đề cần được giải đáp, giúp đỡ chúng mình sẽ cố gắng phản hồi trong thời gian sớm nhất."
+    // };
+    response = math[0].q;
+  } else if (payload === 'exam') {
     response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Chọn chức năng",
-            "subtitle": "Chọn một chức năng để bot trả lời cho bạn",
-            "buttons": [{
-                "type": "postback",
-                "title": "Thông tin về kỳ thi THPT Quốc gia",
-                "payload": "info_thptqg"
-              },
-              {
-                "type": "postback",
-                "title": "Thông tin về kỳ thi Đánh giá năng lực",
-                "payload": "info_dgnl"
-              },
-              {
-                "type": "postback",
-                "title": "Thông tin về ViNiX bot",
-                "payload": "info_vinix"
-              }
-            ],
-          }]
+      "text": "Chọn môn học bạn muốn thi:",
+      "quick_replies": [
+        {
+          "content_type": "text",
+          "title": "Toán",
+          "payload": "Math"
+        }, {
+          "content_type": "text",
+          "title": "Lý",
+          "payload": "Physic",
         }
-      }
-    };
-  } else if (payload === 'info_thptqg') {
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": "Thông tin về kỳ thi THPT Quốc gia được chúng mình tổng hợp ở link sau:",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "THPTQG",
-            "webview_height_ratio": "full"
-          }]
-        }
-      }
-    };
-  } else if (payload === 'info_dgnl') {
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": "Thông tin về kỳ thi Đánh giá năng lực được chúng mình tổng hợp ở link sau:",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Đánh giá năng lực",
-            "webview_height_ratio": "full"
-          }]
-        }
-      }
-    };
-  } else if (payload === 'info_vinix') {
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": "ViNiX được phát triển bởi nhóm BKUFC-THPT Đức Hòa, tham khảo thêm tại link sau:",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://github.com/tien99",
-            "title": "GitHub",
-            "webview_height_ratio": "full"
-          }]
-        }
+      ]
+    }
+  } else if (payload === 'Math') {
+    response = math[Math.floor(Math.random() * 3)].q
+  } else {
+    var ans = payload.toString().split("_");
+    if (math[parseInt(ans[1])].a == ans[2]) {
+      response = math[Math.floor(Math.random() * 3)].q
+    } else {
+      response = {
+        "text": "Sai roi!"
       }
     }
   }
@@ -164,7 +207,7 @@ function callSendAPI(sender_psid, response) {
   }
 
   request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "uri": "https://graph.facebook.com/v6.0/me/messages",
     "qs": {
       "access_token": PAGE_ACCESS_TOKEN
     },
@@ -182,7 +225,7 @@ function callSendAPI(sender_psid, response) {
 function menu() {
   console.log('menu');
   request({
-    "uri": "https://graph.facebook.com/v3.2/me/messenger_profile",
+    "uri": "https://graph.facebook.com/v6.0/me/messenger_profile",
     "qs": {
       "access_token": PAGE_ACCESS_TOKEN
     },
@@ -191,20 +234,11 @@ function menu() {
       "persistent_menu": [{
         "locale": "default",
         "composer_input_disabled": false,
-        "call_to_actions": [{
-            "type": "postback",
-            "title": "Kỳ thi THPT Quốc gia",
-            "payload": "info_thptqg"
-          },
+        "call_to_actions": [
           {
             "type": "postback",
-            "title": "Kỳ thi Đánh giá năng lực",
-            "payload": "info_dgnl"
-          },
-          {
-            "type": "postback",
-            "title": "Thông tin về ViNiX bot",
-            "payload": "info_vinix"
+            "title": "Làm thử một số bài thi",
+            "payload": "exam"
           }
         ]
       }]
@@ -221,14 +255,14 @@ function menu() {
 function start() {
   console.log('start');
   request({
-    "uri": "https://graph.facebook.com/v3.2/me/messenger_profile",
+    "uri": "https://graph.facebook.com/v6.0/me/messenger_profile",
     "qs": {
       "access_token": PAGE_ACCESS_TOKEN
     },
     "method": "POST",
     "json": {
       "get_started": {
-        "payload": "ViNiX"
+        "payload": "GetStarted"
       }
     }
   }, (err, res, body) => {
